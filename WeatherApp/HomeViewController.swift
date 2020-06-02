00d8fd83d60c938ddb2c477dc540e21cdb2df15d
Krystian Barczak <48355396+kryst1anb@users.noreply.github.com>
2020-06-02 16:33:27 +0200

    Working buttons geo location and city name
    TODO: Repair JSON Parser!

--- WeatherApp/HomeViewController.swift
+++ WeatherApp/HomeViewController.swift
@@ -7,23 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var cityInput: UITextField!
     @IBOutlet var BTN_Check: UIButton!
-    
     @IBOutlet weak var BTN_Localization: UIButton!
 
+    var cityName = ""
+    var flag = false
+    var lat_local = ""
+    var lon_local = ""
     
     
-    
-    @IBAction func SearchButton(_ sender: AnyObject) {
-        print("pressed");
-        self.performSegue(withIdentifier: "SecondScreenView", sender: self)
-    }
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         BTN_Check.setTitleColor(UIColor(named: "ColorTextLD"), for: .normal)
         BTN_Check.tintColor = UIColor(named: "ColorTextLD")
@@ -31,9 +33,45 @@ class HomeViewController: UIViewController {
         
         BTN_Localization.setTitleColor(UIColor(named: "ColorTextLD"), for: .normal)
         BTN_Localization.tintColor = UIColor(named: "ColorTextLD")
+    
+    }
+    
+    @IBAction func SearchButton(_ sender: AnyObject) {
+           print("pressed");
+           flag = true
+           self.cityName = cityInput.text!
+           
+           self.performSegue(withIdentifier: "SecondScreenView", sender: self)
+       }
+       
+       @IBAction func LocalizationButton(_ sender: Any) {
+           print("localization")
+           flag = false
         
+        let locManager = CLLocationManager()
+        locManager.requestWhenInUseAuthorization()
         
+        let currentLocation: CLLocation!
+
+        if
+           CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+           CLLocationManager.authorizationStatus() ==  .authorizedAlways
+        {
+            currentLocation = locManager.location
+            self.lon_local = "\(currentLocation.coordinate.longitude)"
+            self.lat_local = "\(currentLocation.coordinate.latitude)"
+        }
         
+           
+           
+       }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as? SecondScreenViewController
+        vc?.finalCityName = self.cityName
+        vc?.finalflag = self.flag
+        vc?.finalLat = self.lat_local
+        vc?.finalLon = self.lon_local
     }
 
 }
--- WeatherApp/SecondScreenViewController.swift
+++ WeatherApp/SecondScreenViewController.swift
@@ -18,21 +18,35 @@ class SecondScreenViewController: UIViewController {
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var descriptionWeather: UILabel!
     
+    var finalCityName = ""
+    var finalflag = false
+    var finalUrl = ""
+    var finalLat = ""
+    var finalLon = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NSSetUncaughtExceptionHandler { exception in
-           print(exception)
-           print(exception.callStackSymbols)
+        print("loaded view")
+        print(finalCityName)
+        print(finalLon)
+        print(finalLat)
+        
+        if finalflag == true{
+            finalUrl = "https://api.openweathermap.org/data/2.5/forecast?q=\(finalCityName)&units=metric&appid=b314601205f4c241d94fa31f7c339a88"
+        }
+        else{
+            finalUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=\(finalLat)&lon=\(finalLon)&units=metric&appid=b314601205f4c241d94fa31f7c339a88"
         }
         
-        guard let url = URL(string: "https://samples.openweathermap.org/data/2.5/find?q=London&appid=439d4b804bc8187953eb36d2a8c26a02") else {return}
+        guard let url = URL(string: "\(finalUrl)") else {return}
             let task = URLSession.shared.dataTask(with: url){(data, response, error) in
                 if let data = data, error == nil{
                     do{
-                        guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {return}
-                        guard let weatherDetails = json["weather"] as? [[String: Any]], let weatherMain = json["main"] as? [String: Any] else {return}
+                        guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]
+                            else {return}
+                        guard let weatherDetails = json["list"] as? [[String: Any]], let weatherMain = json["main"] as? [String: Any]
+                            else {return}
                         let temp = Int(weatherMain["temp"] as? Double ?? 0)
                         let description = (weatherDetails.first?["description"] as? String)?.capitalizingFirstLetter()
                         DispatchQueue.main.async{
@@ -44,6 +58,7 @@ class SecondScreenViewController: UIViewController {
                 }
             }
             task.resume()
+        print("task resume")
         }
         func setWeather(weather: String?,description: String?, temp: Int){
             descriptionWeather.text = description ?? "---"
